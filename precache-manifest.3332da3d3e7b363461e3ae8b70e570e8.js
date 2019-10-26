6df8bcfdc4a5a186df7c326dbddfcb3d177e2843
Michael Gwynne <michael@votemike.co.uk>
2019-10-26 10:45:57 +0100

    Updates

---
+++ precache-manifest.3332da3d3e7b363461e3ae8b70e570e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a034796b0c062677885b68824318d1aa",
+    "url": "/stress-test/index.html"
+  },
+  {
+    "revision": "33e40c66d8ea3326ede8",
+    "url": "/stress-test/static/css/main.ee009875.chunk.css"
+  },
+  {
+    "revision": "42322741993850376c58",
+    "url": "/stress-test/static/js/2.3dcd7bd2.chunk.js"
+  },
+  {
+    "revision": "33e40c66d8ea3326ede8",
+    "url": "/stress-test/static/js/main.46c7bf55.chunk.js"
+  },
+  {
+    "revision": "b24edc51efd5b37e0b7a",
+    "url": "/stress-test/static/js/runtime-main.ca316bac.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/stress-test/static/media/logo.25bf045c.svg"
+  }
+]);
